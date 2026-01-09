629bac4064888e32e5eb9b843d8fe8ace365d90f
Rodrigo Sanchez <sanxcruro122@gmail.com>
2026-01-08 22:58:55 -0600

    Dockerfile y controller arreglado

--- grails-app/controllers/rodnix/escolar/MainController.groovy
+++ grails-app/controllers/rodnix/escolar/MainController.groovy
@@ -3,24 +3,26 @@ package rodnix.escolar
 class MainController {
 
     def index() {
+        // La barra "/" al inicio es OBLIGATORIA.
+        // Busca en: grails-app/views/index.gsp
+        render(view: "/index")
     }
 
     def save() {
         def nombreUsuario = params.nombre
-        
         if(nombreUsuario) {
             try {
                 def nuevo = new Prospecto(nombre: nombreUsuario)
                 if(nuevo.save(flush: true)) {
-                    render(view: "index", model: [mensaje: "¡Guardado exitosamente en Neubox!"])
+                    render(view: "/index", model: [mensaje: "Guardado OK"])
                 } else {
-                    render(view: "index", model: [error: "Error al guardar: " + nuevo.errors])
+                    render(view: "/index", model: [error: "Error: " + nuevo.errors])
                 }
             } catch (Exception e) {
-                render(view: "index", model: [error: "Error de conexión a BD: " + e.message])
+                render(view: "/index", model: [error: "Error BD: " + e.message])
             }
         } else {
-            render(view: "index", model: [error: "Debes escribir un nombre"])
+            render(view: "/index", model: [error: "Pon nombre"])
         }
     }
 }
