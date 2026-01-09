e7bbe174a3ad8702d5dc589ddfe618a901974d7d
Rodrigo Sanchez <sanxcruro122@gmail.com>
2026-01-08 22:10:25 -0600

    arreglo de controller y rutas

--- grails-app/controllers/rodnix/escolar/MainController.groovy
+++ grails-app/controllers/rodnix/escolar/MainController.groovy
@@ -3,23 +3,24 @@ package rodnix.escolar
 class MainController {
 
     def index() {
-        // Renderiza la vista index.gsp
     }
 
     def save() {
-        // Recibe los datos del formulario
         def nombreUsuario = params.nombre
-        // Verificamos el captcha (opcional por ahora para prueba rapida)
         
         if(nombreUsuario) {
-            def nuevo = new Prospecto(nombre: nombreUsuario)
-            if(nuevo.save(flush: true)) {
-                render "<h1>¡Guardado!</h1><p>El usuario ${nombreUsuario} está en la BD de Neubox.</p><a href='/'>Volver</a>"
-            } else {
-                render "Error al guardar: " + nuevo.errors
+            try {
+                def nuevo = new Prospecto(nombre: nombreUsuario)
+                if(nuevo.save(flush: true)) {
+                    render(view: "index", model: [mensaje: "¡Guardado exitosamente en Neubox!"])
+                } else {
+                    render(view: "index", model: [error: "Error al guardar: " + nuevo.errors])
+                }
+            } catch (Exception e) {
+                render(view: "index", model: [error: "Error de conexión a BD: " + e.message])
             }
         } else {
-            render "No enviaste nombre."
+            render(view: "index", model: [error: "Debes escribir un nombre"])
         }
     }
 }
